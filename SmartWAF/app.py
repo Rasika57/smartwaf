6985e732aeecd8d31121e132d6ec338bdb7dd9f7
Rasika Talegaonkar <multiverse.of.skills77@gmail.com>
2025-11-14 23:39:23 +0530

    Initial commit

--- SmartWAF/app.py
+++ SmartWAF/app.py
@@ -89,4 +89,7 @@ def smartadmin():
     return render_template("dashboard.html", rows=rows)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+    # app.run(debug=True)
